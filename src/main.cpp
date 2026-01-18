234d5ccb0c61840f360e7639bc49ece1f8ba8ea0
marouaneMJH <marwanmoujahid008@gmail.com>
2026-01-19 00:57:20 +0100

    feat: update benchmark results format and enhance main function structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "core/node.hpp"
+#include "core/cli_manager.hpp"
 #include "benchmark/benchmark.hpp"
 
 #define PORT_BASE 10000
@@ -10,22 +11,26 @@ int main(int argc, char *argv[])
   if (argc >= 2)
     nbr_nodes = std::stoi(argv[1]);
 
+  CliManager cli;
+
+  cli.run();
+
   // --- BENCHMARK ---
-  auto nodes = Benchmark::generate_nodes(nbr_nodes, PORT_BASE);
-  Benchmark bench(nodes, MINUTES(1));
+  // auto nodes = Benchmark::generate_nodes(nbr_nodes, PORT_BASE);
+  // Benchmark bench(nodes, MINUTES(1));
 
-  bench.start();
-  auto result = bench.wait_and_collect();
+  // bench.start();
+  // auto result = bench.wait_and_collect();
 
-  std::cout
-      << "sent=" << result.sent
-      << " recv=" << result.received
-      << " drop=" << result.dropped
-      << " msg/s=" << result.msg_per_sec
-      << " p50(ns)=" << result.p50_ns
-      << " p95(ns)=" << result.p95_ns
-      << " p99(ns)=" << result.p99_ns
-      << "\n";
+  // std::cout
+  //     << "sent=" << result.sent
+  //     << " recv=" << result.received
+  //     << " drop=" << result.dropped
+  //     << " msg/s=" << result.msg_per_sec
+  //     << " p50(ns)=" << result.p50_ns
+  //     << " p95(ns)=" << result.p95_ns
+  //     << " p99(ns)=" << result.p99_ns
+  //     << "\n";
 
   return 0;
 }
